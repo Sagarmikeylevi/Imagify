419ca14a68e2414a17f151e8124118dbd121b54f
Sagar das <sagarmikeylevi@gmail.com>
2024-04-24 11:46:06 +0530

    ...

---
+++ components/MediaUploader.tsx
@@ -0,0 +1,37 @@
+import { useToast } from "./ui/use-toast";
+import { CldUploadWidget } from "next-cloudinary";
+
+const MediaUploader = () => {
+  const { toast } = useToast();
+
+  const onUploadSuccessHandler = (result: any) => {
+    toast({
+      title: "Image uploaded successfully",
+      description: "1 credit was deducted from your account",
+      duration: 5000,
+      className: "success-toast",
+    });
+  };
+
+  const onUploadErrorHandler = () => {
+    toast({
+      title: "Something went wrong while uploading",
+      description: "Please try again",
+      duration: 5000,
+      className: "error-toast",
+    });
+  };
+  return (
+    <CldUploadWidget
+      uploadPreset="jsm_imaginify"
+      options={{
+        multiple: false,
+        resourceType: "image",
+      }}
+      onSuccess={onUploadSuccessHandler}
+      onError={onUploadErrorHandler}
+    ></CldUploadWidget>
+  );
+};
+
+export default MediaUploader;
